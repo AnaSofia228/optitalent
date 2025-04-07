f38ab9d36a309e56814fda85f6b38b8e526dd835
AnaSofia228 <anasofiaricardoaguilar@gmail.com>
2025-04-06 21:48:32 -0500

    first commit: Optitalent Projects

---
+++ optitalent/src/main/java/com/example/optitalent/OptitalentApplication.java
@@ -0,0 +1,13 @@
+package com.example.optitalent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptitalentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptitalentApplication.class, args);
+	}
+
+}
---
+++ optitalent/src/test/java/com/example/optitalent/OptitalentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.optitalent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptitalentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
