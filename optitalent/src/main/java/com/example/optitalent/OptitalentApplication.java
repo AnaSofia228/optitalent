78b8254673c063cafec9a75fc4da919b0567d356
juanjo-villa <134659772+juanjo-villa@users.noreply.github.com>
2025-04-06 22:01:20 -0500

    Creation of all files

--- optitalent/src/main/java/com/example/optitalent/OptitalentApplication.java
+++ optitalent/src/main/java/com/example/optitalent/OptitalentApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OptitalentApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OptitalentApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OptitalentApplication.class, args);
+    }
 
 }
